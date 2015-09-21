6f24c12dabdd31976bd92bce5ffa2dd0fc83bcb9
amit <amitshob@email.com>
2015-09-20 18:19:15 -0700

    add specs and initial files

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function Pizza(quantity, toppings, pizzaSize){
+this.quantity = quantity;
+this.toppings = toppings;
+this.pizzaSize = pizzaSize;
+}
+
+Pizza.prototype.cost = function() {
+
+  total_cost = quantity*(pizza_cost);
+  pizza_cost = size + length.toppings*
+
+}
---
+++ spec/specs.js
@@ -0,0 +1,14 @@
+describe('Pizza', function() {
+  it('creates a new pizza with the given attributes ', function() {
+    var testPizza = new Pizza(2,["cheese","veggies"], "small");
+    expect(testPizza.quantity).to.eql(2);
+    expect(testPizza.toppings).to.eql(["cheese","veggies"]);
+    expect(testPizza.pizzaSize).to.equal("small");
+  });
+
+  it('returns the total cost of the order', function() {
+    var testPizza = new pizza(2,["cheese","veggies"], "small");
+    expect(testPizza.cost()).to.equal(tbd)
+  });
+
+});
