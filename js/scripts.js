0d29e7361d4f5442cc33e82311b7c6a41d47240e
amit <amitshob@email.com>
2015-09-22 00:47:01 -0700

    CSS effect, jquery

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ this.pizzaSize = pizzaSize;
 
 Pizza.prototype.cost = function() {
   var total_cost = 0;
+  var this.toppings = this.toppings.split(",");
   if (this.pizzaSize === "small") {
     total_cost = this.quantity*((6 + (this.toppings.length)*1));
   } else if (this.pizzaSize === "medium"){
@@ -15,3 +16,20 @@ Pizza.prototype.cost = function() {
   }
   return total_cost;
 };
+
+$(function(){
+  $("form.pizza-selections").submit(function(event){
+    event.preventDefault();
+
+    var pizza-quantity = parseInt($("#pizza-quantity").val());
+    var pizza-toppings = $("#pizza-toppings").val();
+    var size = $("select#size").val();
+    var testPizza = new Pizza(quantity, toppings, pizzaSize);
+    var cost = testPizza.cost();
+
+    $("#order-cost").text(cost);
+    $("#total-cost").show();
+
+  })
+
+})
