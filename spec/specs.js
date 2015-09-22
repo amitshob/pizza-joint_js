8f3f0c8ea10ab7fdf6aae992e865aa29bbac73aa
amitshob <amitshob@gmail.com>
2015-09-22 13:38:04 -0700

    Update specs.js

--- spec/specs.js
+++ spec/specs.js
@@ -1,13 +1,13 @@
 describe('Pizza', function() {
   it('creates a new pizza with the given attributes ', function() {
-    var testPizza = new Pizza(2,["cheese","veggies"], "small");
+    var testPizza = new Pizza(2,"cheese,veggies", "small");
     expect(testPizza.quantity).to.eql(2);
-    expect(testPizza.toppings).to.eql(["cheese","veggies"]);
+    expect(testPizza.toppings).to.eql("cheese,veggies");
     expect(testPizza.pizzaSize).to.equal("small");
   });
 
   it('returns the total cost of the order', function() {
-    var testPizza = new Pizza(2, ["cheese","veggies"], "small");
+    var testPizza = new Pizza(2, "cheese,veggies", "small");
     expect(testPizza.cost()).to.equal(16);
   });
 
